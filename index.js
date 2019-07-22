afbc79e372698756ce8e91999d1554ba2897e810
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-22 14:20:52 +0100

    feat: implement login functionality

--- index.js
+++ index.js
@@ -29,5 +29,22 @@ server.post('/api/register', (req, res) => {
     });
 });
 
+server.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
