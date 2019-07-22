ceb46ed48726abfada779c1de96fd4c851479f72
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-22 13:35:51 +0100

    feat: implement register with bcrypt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 const express = require('express');
 const helmet = require('helmet');
+const bcrypt = require('bcryptjs');
+const Users = require('./users/users-model.js');
 
 const server = express();
 
@@ -9,6 +11,22 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
+});
+
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json({
+        id: saved.id,
+        username: saved.username,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
 
 const port = process.env.PORT || 5000;
