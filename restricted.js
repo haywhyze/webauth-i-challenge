795d6b5bb569c42b224cc473971718d37bb1b837
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-23 15:33:45 +0100

    refactor restricted middleware

--- restricted.js
+++ restricted.js
@@ -1,24 +1,8 @@
-const Users = require('./users/users-model.js');
-const sillyBcrypt = require('./sillyBcrypt');
-
 function restricted(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then((user) => {
-        if (user && sillyBcrypt.compare(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch((error) => {
-        res.status(500).json({ message: 'Unexpected error' });
-      });
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(401).json({ message: 'you shall not pass!!' });
   }
 }
 
